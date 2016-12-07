abb9367d9aebae1f152c15392d9455157a355a2b
Wan Jing Chuan <11355431@qq.com>
2016-12-07 18:12:11 +0800

    update...

---
+++ Client/Assets/BackgroundController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundController : MonoBehaviour {
+
+    public float moveSpeed = 5;
+    public Renderer rend;
+
+    // Use this for initialization
+    void Start () {
+
+        rend = GetComponent<Renderer>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        rend.material.mainTextureOffset += new Vector2(Time.deltaTime * moveSpeed * 0.02f, 0);
+    }
+}
